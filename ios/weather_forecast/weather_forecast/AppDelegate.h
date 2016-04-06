9fcac9f28c0986876580ef7d58f46ffc64071477
andrew <andrew@andrews-MacBook-Pro.local>
2016-04-06 08:14:36 -0700

    ios client init

---
+++ ios/weather_forecast/weather_forecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather_forecast
+//
+//  Created by andrew on 4/6/16.
+//  Copyright © 2016 andrewm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/weather_forecast/weather_forecast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weather_forecast
+//
+//  Created by andrew on 4/6/16.
+//  Copyright © 2016 andrewm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
