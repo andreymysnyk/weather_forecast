ce343bd22affc814aeb3779917d8df83c351cf01
andrew <andrew@andrews-MacBook-Pro.local>
2016-04-08 18:41:51 +0300

    implementing cities list

---
+++ ios/weather_forecast/weather_forecast/PickerDataSource.h
@@ -0,0 +1,13 @@
+//
+//  PickerDataSource.h
+//  weather_forecast
+//
+//  Created by andrew on 4/8/16.
+//  Copyright © 2016 andrewm. All rights reserved.
+//
+
+#ifndef PickerDataSource_h
+#define PickerDataSource_h
+
+
+#endif /* PickerDataSource_h */
---
+++ ios/weather_forecast/weather_forecast/PickerDataSource.m
@@ -0,0 +1,9 @@
+//
+//  PickerDataSource.m
+//  weather_forecast
+//
+//  Created by andrew on 4/8/16.
+//  Copyright © 2016 andrewm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
